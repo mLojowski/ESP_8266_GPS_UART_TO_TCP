ae4afec089dc9f90d02c2dca458cdc7f2060cf7b
Marcin Lojowski <marcin.lojowski@aptiv.com>
2024-07-29 20:45:00 +0200

    [ESP_8266_GPS_UART_TO_TCP] NMEA adds + cleanup

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 ESP_8266_GPS_UART_TO_TCP
 
+Credits:
+UART -> https://docs.arduino.cc/learn/built-in-libraries/software-serial/
+NMEA_0183 Multiplexer -> https://github.com/AK-Homberger/NMEA0183-WiFi-Multiplexer/tree/main
+NMEA_0183 LIB -> https://github.com/ttlappalainen/NMEA0183
+ESP_telnet example -> https://github.com/esp8266/Arduino/issues/1169 (tcp info)
+
+
 INITIAL COMMIT: d89e6666d33c05f7785f6168dce31eb38ba97388
 
 SoftwareSerial configuration for pins 
@@ -11,46 +18,12 @@ Ser2_TX_PIN 13
 
 + UARTS_Verification method with 
 EXAMPLE OUTPUT:
-1>1
-2>1
-1>1
-2>1
+
+ ESP8266 Software serial test started
 1>1
 2>1
 1>2
 2>2
-1>2
-2>2
-1>2
-2>2
 1>3
 2>3
-1>3
-2>3
-1>3
-2>3
-1>4
-2>4
-1>4
-2>4
-1>4
-2>4
-1>5
-2>5
-1>5
-2>5
-1>5
-2>5
-1>6
-2>6
-1>6
-2>6
-1>6
-2>6
-1>6
-2>6
-1>6
-2>6
-1>7
-2>7
 
--- serial_init.ino
+++ serial_init.ino
@@ -1,28 +1,74 @@
 #include <SoftwareSerial.h>
 #include <time.h>
-// pins Rx GPIO14 (D5) and Tx GPIO 12 (D6)
+
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+
+#include "NMEA0183/NMEA0183.h"
+//Rewrite to header:
+//#include "NMEA0183-WiFi-Multiplexer/NMEA083-WiFi-Multiplexer"
+
+//MACRO DEFNITIONS - UART's
+//----------------------------------------------
+// Serial = DEBUG
+#define DEBUG_Baud 115200
+
+// Ser1 = GPS_SERIAL
 #define Ser1_RX_PIN 5
 #define Ser1_TX_PIN 4
+#define Ser1_Baud 4800
 
+// Ser2 = GPS_SERIAL
 #define Ser2_RX_PIN 12
 #define Ser2_TX_PIN 13
+#define Ser2_Baud 9600
+//---------------------------------------------
 
+
+#define MAX_NMEA0183_MESSAGE_SIZE 150
+char buf1[MAX_NMEA0183_MESSAGE_SIZE] = "";    // Buffer for serial#1
+char buf2[MAX_NMEA0183_MESSAGE_SIZE] = "";    // Buffer for serial#2
+
+// Create software serial objects
 SoftwareSerial swSer1(Ser1_RX_PIN, Ser1_TX_PIN);
 SoftwareSerial swSer2(Ser2_RX_PIN, Ser2_TX_PIN);
 
-char ch = 'q';
 
-void setup() {
-  Serial.begin(115200);  //Initialize hardware serial with baudrate of 115200
-  swSer1.begin(4800);    //Initialize software serial with baudrate of 4800
-  swSer2.begin(9600);   //Initialize software serial with baudrate of 9600
-  Serial.println("\nESP8266 Software serial test started");
 
-    ets_delay_us(100);
+
+void setup(void) {
+
+  //Debug serial config
+  Serial.begin(DEBUG_Baud);                   //Initialize hardware serial with baudrate of 115200
+
+  //Ser1 config
+  pinMode(Ser1_RX_PIN, INPUT);
+  pinMode(Ser1_TX_PIN, OUTPUT);
+  swSer1.enableIntTx(false);
+  swSer1.begin(Ser1_Baud);                    //Initialize software serial with baudrate of 4800
+
+  //Ser2 config
+  pinMode(Ser2_RX_PIN, INPUT);
+  pinMode(Ser2_TX_PIN, OUTPUT);
+  swSer2.enableIntTx(false);
+  swSer2.begin(Ser2_Baud);                    //Initialize software serial with baudrate of 9600
+
+
+  //INIT:
+  Serial.println("\nESP8266 Software serial config started");
+
+
 }
+
+
+
+
+
+
 
 void UARTS_Verification()
 {
+  char ch = 'q';
   while (true)
   {
     while (Serial.available() > 0) {  //wait for data at hardware serial
@@ -43,8 +89,10 @@ void UARTS_Verification()
   }
 }
 
+
 void loop()
 {
-UARTS_Verification();
+  UARTS_Verification();
+
 
 }
